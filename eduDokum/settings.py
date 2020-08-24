e501f3468c8ca8a51222c4c32d6f515981b888c7
Uzair <m.uzairrazzaq@gmail.com>
2020-08-24 16:57:23 +0500

    Signed-off-by: Muhammad Uzair Razzaq <m.uzairrazzaq@gmail.com>

--- eduDokum/settings.py
+++ eduDokum/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'w0@@t994^#+5nxrm2qqtil7l$o^aut)&u^*e&nip-y22a3utow'
+with open('/etc/secret_key.txt') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
