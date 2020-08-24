58f4fd92df0d4ab380bb2c2d0b05210909daffe3
Uzair <m.uzairrazzaq@gmail.com>
2020-08-24 17:08:35 +0500

    removed seceret key

--- eduDokum/settings.py
+++ eduDokum/settings.py
@@ -20,8 +20,9 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-with open('/etc/secret_key.txt') as f:
-    SECRET_KEY = f.read().strip()
+import os
+
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
